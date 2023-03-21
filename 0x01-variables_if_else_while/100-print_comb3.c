05d496f0954b5dd1e32c3207b407c4ef7ce1c7bf
dtemilade <dtemilade@gmail.com>
2023-03-20 21:42:03 -0700

    combination for two digits commit

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -7,5 +7,33 @@
  */
 int main(void)
 {
-return (0);
+
+	int f_digit, s_digit;
+
+	for (f_digit = 48; f_digit <= 56; f_digit++)
+
+	{
+
+		for (s_digit = 49; s_digit <= 57; s_digit++)
+
+		{
+			if (f_digit < s_digit)
+
+			{
+				putchar(s_digit);
+				putchar(f_digit);
+
+				if (s_digit != 56 || f_digit != 57)
+
+				{
+					putchar(',');
+					putchar(' ');
+				}
+			}
+		}
+	}
+
+
+	putchar('\n');
+	return (0);
 }
