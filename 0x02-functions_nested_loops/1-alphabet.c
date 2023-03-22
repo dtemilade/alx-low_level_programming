a12d2277a7eaf3071bf28d97afe631d5ae468775
dtemilade <dtemilade@gmail.com>
2023-03-21 17:26:05 -0700

    alphabet lowercase commit

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+* main - check the code
+* print_alphabet - This function prints the alphabet on screen.
+* Return: Always 0.
+*/
+void print_alphabet(void)
+{
+	char alph;
+
+	for (alph = 'a' ; alph <= 'z'; alph++)
+	_putchar(alph);
+	_putchar('\n');
+}
