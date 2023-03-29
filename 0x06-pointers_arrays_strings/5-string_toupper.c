ca3201d19340907c8f48109913e08e6ab96c5ded
dtemilade <dtemilade@gmail.com>
2023-03-29 10:56:24 -0700

    0x06. C - More pointers, arrays and strings mandatory task commits

---
+++ 0x06-pointers_arrays_strings/5-string_toupper.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * string_toupper - lowercase string to uppercase string
+ * @n: string pointer for changes
+ *
+ * Return: n
+ */
+char *string_toupper(char *n)
+{
+	int x;
+
+	x = 0;
+	while (n[x] != '\0')
+	{
+		if (n[x] >= 'a' && n[x] <= 'z')
+			n[x] = n[x] - 32;
+		x++;
+	}
+	return (n);
+}
---
+++ 0x06-pointers_arrays_strings/README.md
@@ -0,0 +1,2 @@
+#!/bin/bash
+My README file for 0x06. C - More pointers, arrays and strings.
