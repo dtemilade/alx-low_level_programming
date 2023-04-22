032eb307be263eab4e2ef89fe3f98c82d1e211b4
dtemilade <dtemilade@gmail.com>
2023-04-21 17:50:00 -0700

    correction and other submission on my 0x0F-function_pointers project

---
+++ 0x0F-function_pointers/100-main_opcodes.c
@@ -0,0 +1,50 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main - program that prints the opcodes of its own main function
+ * @argc: parameter for number of arguments in the program
+ * @argv: parameter for array of arguments in the program
+* Return: Always 0 (Success)
+ */
+
+int main(int argc, char *argv[])
+{
+	/*introducing new variables*/
+	char *var_ptr;
+	int a, b;
+
+	/*incorrect argument print Error, then new line and 1 for exit status*/
+	if (argc < 2 || argc > 2)
+	{
+		printf("Error\n");
+		exit(1);
+	}
+
+	/* negative number print Error, then new line and 2 for exit status*/
+	b = atoi(argv[1]);
+
+	if (b < 0)
+	{
+		printf("Error\n");
+		exit(2);
+	}
+
+	/*process to determine the opcodes of main*/
+	var_ptr = (char *)main;
+
+	for (a = 0; a < b; a++)
+	{
+		/*using b minus 1 to compare a*/
+		if (a == b – 1)
+		{
+			/*stop the loop and put new line when the if condition meets*/
+			printf("%02hhx\n", var_ptr[a]);
+			break;
+		}
+		/*else give space after each two char long opcode*/
+		printf("%02hhx ", var_ptr[a]);
+	}
+	return (0);
+}
+
--- 0x0F-function_pointers/3-main.c
+++ 0x0F-function_pointers/3-main.c
@@ -12,12 +12,14 @@
 
 int main(int __attribute__((__unused__)) argc, char *argv[])
 {
-	int a, b;
+	/*introducing variable parameter and pointer*/
+	int b, a;
 	char *arr_ptr;
 
+	/*condition for error and its outputs*/
 	if (argc != 4)
 	{
-		printf("ErrorOccured \n");
+		printf("Error\n");
 		exit(98);
 	}
 	a = atoi(argv[1]);
@@ -25,15 +27,16 @@ int main(int __attribute__((__unused__)) argc, char *argv[])
 	b = atoi(argv[3]);
 	if (get_op_func(arr_ptr) == NULL || arr_ptr[1] != '\0')
 	{
-		printf("ErrorOccured \n");
+		printf("Error\n");
 		exit(99);
 	}
 	if ((*arr_ptr == '/' && b == 0) ||
 		    (*arr_ptr == '%' && b == 0))
 	{
-		printf("ErrorOccured\n");
+		printf("Error\n");
 		exit(100);
 	}
+	/*absence of error, then the output*/
 	printf("%d\n", get_op_func(arr_ptr)(a, b));
 	return (0);
 }
